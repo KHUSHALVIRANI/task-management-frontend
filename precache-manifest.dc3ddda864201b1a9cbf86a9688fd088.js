309b4286310ec07e2290e8c8277fdaa301cd214d
KHUSHAL <kppatel7606@gmail.com>
2022-04-14 15:21:38 +0530

    Updates

---
+++ precache-manifest.dc3ddda864201b1a9cbf86a9688fd088.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24daaeeae2b99eea97314139b2a8869d",
+    "url": "/task-management-frontend/index.html"
+  },
+  {
+    "revision": "21a682d2079a0115bfb5",
+    "url": "/task-management-frontend/static/css/main.0a97c640.chunk.css"
+  },
+  {
+    "revision": "6f1617cf399d18b5853d",
+    "url": "/task-management-frontend/static/js/2.12f2b314.chunk.js"
+  },
+  {
+    "revision": "21a682d2079a0115bfb5",
+    "url": "/task-management-frontend/static/js/main.28e22ae7.chunk.js"
+  },
+  {
+    "revision": "170cb679b8a6db4918f5",
+    "url": "/task-management-frontend/static/js/runtime~main.961a32b0.js"
+  }
+]);
